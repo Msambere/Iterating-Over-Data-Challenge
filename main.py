3825996bf564ed587d1ef9b6cd2478559447a404
Amber <110812972+Msambere@users.noreply.github.com>
2024-09-12 13:41:00 -0500

    Current state of repo

--- main.py
+++ main.py
@@ -1,11 +1,25 @@
 def calculate_total_sales(sales_totals):
-    pass
+    if not sales_totals:
+        return 0.0
+    total = 0
+    for candy_name, candy_details in sales_totals.items():
+         total += candy_details["quantity"] * candy_details["price"]
+    return total
+
 
 def calculate_average_sales(sales_totals):
-    pass
+    if not sales_totals:
+        return 0.0
+    total = calculate_total_sales(sales_totals)
+    num_of_candies = len(sales_totals)
+    average =  total / num_of_candies
+    return average
+
 
 def filter_to_better_than_average_sales(sales_totals):
-    pass
+    if not sales_totals:
+        return {}
     
 def ninety_nine_bottles(count, beverage):
     pass
+
